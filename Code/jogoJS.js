80d01ffb1af6deb119d98f3f20530a2dce3a4b1f
Willian Grando <Williangrando@hotmail.com>
2020-06-12 20:01:11 -0300

    teste de funções

--- Code/jogoJS.js
+++ Code/jogoJS.js
@@ -1,2 +1,18 @@
-var num1, num2, pisicao;
-posicao = num1 * num2;
+var num1=0, num2=0, pisicao=0;
+var player=0;
+
+function teste(){
+//função de calculo para posição
+    num1 = document.getElementById("num1").value;
+    num1 = Number(num1);
+    num2 = document.getElementById("num2").value;
+    num2 = Number(num2);
+    posicao = num1 * num2;
+//função de player com cores
+    if(player % 2 == 0){
+    document.getElementById(posicao).style.backgroundColor = "red";
+    }else{
+    document.getElementById(posicao).style.backgroundColor = "blue";
+    }
+    player++;
+}
