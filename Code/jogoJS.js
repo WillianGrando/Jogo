607033b8d8cee1d8b67b78f1f3596b4c8718c374
Willian Grando <Williangrando@hotmail.com>
2020-07-06 19:40:37 -0300

    2.3.3 identação do codigo

--- Code/jogoJS.js
+++ Code/jogoJS.js
@@ -1,4 +1,14 @@
-var player = 0, jogadaX, jogadaY;
+let player = 0,
+    jogadaX,
+    jogadaY,
+    posiX,
+    posiY,
+    num1,
+    num2,
+    posicao,
+    vitoria;
+
+//variavel para o progama "ler" as jogadas
 var jogadas = [
         ["", "", "", "", "", ""],
         ["", "", "", "", "", ""],
@@ -6,11 +16,18 @@ var jogadas = [
         ["", "", "", "", "", ""],
         ["", "", "", "", "", ""],
         ["", "", "", "", "", ""],
-        ];
-let posiX, posiY;
-function teste() {
-    //todo - seletor de numero mais bonito
+    ];
+//variavel para o progama "ler" a tela
+var tela = [
+        [ "1",  "2",  "3",  "4",  "5",  "6"],
+        [ "7",  "8", " 9", "10", "12", "14"],
+        ["15", "16", "18", "20", "21", "24"],
+        ["25", "27", "28", "30", "32", "35"],
+        ["36", "40", "42", "45", "48", "49"],
+        ["54", "56", "63", "64", "72", "81"],
+    ];
 
+function jogada() {
     //calculo para os numeros jogados
     let num1 = document.getElementById('num1').value;
     num1 = Number(num1);
@@ -21,23 +38,15 @@ function teste() {
     //função cores do player
     if (document.getElementById(posicao).style.backgroundColor == '') {//troca a cor na posição jogada
         if (player % 2 == 0) {
-        document.getElementById(posicao).style.backgroundColor = 'red';
+            document.getElementById(posicao).style.backgroundColor = 'red';
         }else {
-        document.getElementById(posicao).style.backgroundColor = 'blue';
+            document.getElementById(posicao).style.backgroundColor = 'blue';
         }
         player++;
     }
-
-    //totest - função vitoria "analisar as 8 direções (nos 4 sentidos) a partir da ultima peça jogada."(horizontal e vertical * falta diagonais)
-    var tela = [//variavel para o progama "ler" a tela
-        [ "1",  "2",  "3",  "4",  "5",  "6"],
-        [ "7",  "8", " 9", "10", "12", "14"],
-        ["15", "16", "18", "20", "21", "24"],
-        ["25", "27", "28", "30", "32", "35"],
-        ["36", "40", "42", "45", "48", "49"],
-        ["54", "56", "63", "64", "72", "81"],
-        ];    
-    for (posiX = 0; posiX < 6; posiX++) {//loop para ler a jogada e o jogador
+    
+    //loop para ler a jogada e o jogador
+    for (posiX = 0; posiX < 6; posiX++) {
         for (posiY = 0; posiY < 6; posiY++) {
             if (tela[posiX][posiY] == posicao) {  
                 if (player % 2 == 0){
@@ -49,55 +58,72 @@ function teste() {
             }
         }
     }
-
 }
 
 function condicaoVitoria() {
-//função pra vitorias na vertical
+    //função pra vitorias na vertical
     var vitoria = 0;
-console.log("testado vertical Vermelho");
+    console.log("testado vertical Vermelho");
     for (var jogodaX = 0; jogodaX < 6; jogodaX++) {//vertical
         if (jogadas[jogodaX][posiY] == 'R') {
             vitoria++;
             if (vitoria == 4) {
                 alert("VERMELHO GANHOU!");
             }
-        }else{vitoria = 0;}
-    }vitoria = 0;
-console.log("testado horizontal vermelho");
+        }else {
+            vitoria = 0;
+        }
+    }
+    vitoria = 0;
 
+    console.log("testado horizontal vermelho");
     for (var jogadaY = 0; jogadaY < 6; jogadaY++) {//horizontal
         if (jogadas[posiX][jogadaY] == 'R') {
             vitoria++;
             if (vitoria == 4) {
                 alert("VERMELHO GANHOU!");
             }
-        }else{vitoria = 0;}
-    }vitoria = 0;
-console.log("testado vertical azul");
+        }else {
+            vitoria = 0;
+        }
+    }
+    vitoria = 0;
 
+    console.log("testado vertical azul");
     for (var jogodaX = 0; jogodaX < 6; jogodaX++) {//vertical
         if (jogadas[jogodaX][posiY] == 'B') {
             vitoria++;
             if (vitoria == 4) {
                 alert("AZUL GANHOU!");
             }
-        }else{vitoria = 0;}
-    }vitoria = 0;
-console.log("testado horizontal azul");
+        }else {
+            vitoria = 0;
+        }
+    }
+    vitoria = 0;
 
+    console.log("testado horizontal azul");
     for (var jogadaY = 0; jogadaY < 6; jogadaY++) {//horizontal
         if (jogadas[posiX][jogadaY] == 'B') {
             vitoria++;
             if (vitoria == 4) {
                 alert("AZUL GANHOU!");
             }
-        }else{vitoria = 0;}
-    }vitoria = 0;
+        }else {
+            vitoria = 0;
+        }
+    }
+    vitoria = 0;
 
-    /*for (posiX = 0; posiX < 6; posiX++) {
+    /*//printa no console o array das jogadas - historico de jogadas - debugger
+    for (posiX = 0; posiX < 6; posiX++) {
         for (posiY = 0; posiY < 6; posiY++) {
             console.log(jogadas[posiX][posiY])
         }
     }*/
+}
+
+function telaInicial() {
+
+    
 }
